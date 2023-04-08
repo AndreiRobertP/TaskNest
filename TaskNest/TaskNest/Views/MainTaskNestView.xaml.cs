7538409664ac2ca530a918a780ca48f86a1769f7
AndreiRobertP <andrei-robert.popa@student.unitbv.ro>
2023-04-08 18:18:50 +0300

    Removed unused using statements

--- TaskNest/TaskNest/Views/MainTaskNestView.xaml.cs
+++ TaskNest/TaskNest/Views/MainTaskNestView.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Media;
 using TaskNest.Models;
 using TaskNest.Services;
 using TaskNest.ViewModels;
